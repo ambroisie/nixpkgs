99bbf9b1a3ebed9ec96a01a3542700645339aa0b
Bruno BELANYI <bruno@belanyi.fr>
2024-07-09 13:05:46 +0000

    nixos/nginx-sso: allow using file-based secrets
    This was living for a *long* time in my config, I finally decided to
    upstream it.

--- nixos/modules/services/security/nginx-sso.nix
+++ nixos/modules/services/security/nginx-sso.nix
@@ -1,11 +1,11 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, utils, ... }:
 
 with lib;
 
 let
   cfg = config.services.nginx.sso;
   format = pkgs.formats.yaml { };
-  configYml = format.generate "nginx-sso.yml" cfg.configuration;
+  configPath = "/var/lib/nginx-sso/config.yaml";
 in {
   options.services.nginx.sso = {
     enable = mkEnableOption "nginx-sso service";
@@ -37,6 +37,11 @@ in {
         nginx-sso configuration
         ([documentation](https://github.com/Luzifer/nginx-sso/wiki/Main-Configuration))
         as a Nix attribute set.
+
+        Options containing secret data should be set to an attribute set
+        containing the attribute `_secret` - a string pointing to a file
+        containing the value the option should be set to. This file should be
+        readable by `nginx-sso`.
       '';
     };
   };
@@ -47,14 +52,29 @@ in {
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
+        StateDirectory = "nginx-sso";
+        WorkingDirectory = "/var/lib/nginx-sso";
+        ExecStartPre = pkgs.writeShellScript "merge-nginx-sso-config" ''
+          rm -f '${configPath}'
+          # Relies on YAML being a superset of JSON
+          ${utils.genJqSecretsReplacementSnippet cfg.configuration configPath}
+        '';
         ExecStart = ''
           ${lib.getExe cfg.package} \
-            --config ${configYml} \
+            --config ${configPath} \
             --frontend-dir ${lib.getBin cfg.package}/share/frontend
         '';
         Restart = "always";
-        DynamicUser = true;
+        User = "nginx-sso";
+        Group = "nginx-sso";
       };
     };
+
+    users.users.nginx-sso = {
+      isSystemUser = true;
+      group = "nginx-sso";
+    };
+
+    users.groups.nginx-sso = { };
   };
 }
